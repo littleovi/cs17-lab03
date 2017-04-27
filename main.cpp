79b1baf6287d3946406b4e279ab7fbf7e832ba1e
u211-03 <u211-03@public.mpei.local>
2017-04-27 14:58:36 +0300

    7 пункт(2 часть)

--- main.cpp
+++ main.cpp
@@ -3,26 +3,10 @@
 #include <sstream>
 #include <cassert>
 #include "tempreture.h"
+
 using namespace std;
 
 
-istream& operator >> (istream& in, tempreture& t){
- in >> t.value;
-    char symbol;
-    in >> symbol;
-    switch(symbol){
-    case 'K':
-        t.scale=Kelvin;
-        break;
-    case 'C':
-        t.scale=Celcium;
-        break;
-    case 'F':
-        t.scale=Farengheite;
-        break;
-    }
-    return in;
-}
 void test_tempreture_input(){
     stringstream in ("17C");
     tempreture t;
@@ -40,44 +24,8 @@ void test_tempreture_input(){
     assert(t.value == 28);
     assert(t.scale == Kelvin);
 }
-  tempreture  convert(const tempreture& from, Scale scale){
-      double K;
-      switch(from.scale){
-      case Kelvin:
-          K =  from.value;
-          break;
-      case Celcium:
-          K =  from.value+273,15;
-          break;
-      case Farengheite:
-          K = (from.value+459,67)/1.8;
-          break;
-      }
-      tempreture result;
-      result.scale = scale;
-      switch(scale){
-      case Kelvin:
-          result.value = Kelvin;
-          break;
-      case Celcium:
-          result.value = Kelvin-273,15;
-      break;
-      case Farengheite:
-          result.value = (Kelvin*1,8)-459,67;
-      break;
-      }
-      return result;
-      //switch по шкале температуры  from,
-      // рассчитать K
-      //switch о шкале to
-      //рассчитать значение результата
-  };
-bool operator < (tempreture& lhs,tempreture& rhs) {
-    tempreture t ;
-    t = convert(lhs, rhs.scale);
-    return t.value < rhs.value ;
 
-}
+
 int
 main() {
     size_t number_count;
